8ba86abd2828c08f56c43f7eab3e774db157899a
Oliver Pinter <oliver.pntr@gmail.com>
2014-04-12 20:14:13 +0200

    PAX Segvguard: style(9) and notes
    Signed-off-by: Oliver Pinter <oliver.pntr@gmail.com>

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -508,7 +508,7 @@ interpret:
 	}
 
 #ifdef PAX_SEGVGUARD
-    error = pax_segvguard(curthread, imgp->vp, args->fname, 0);
+	error = pax_segvguard(curthread, imgp->vp, args->fname, 0);
 #endif
 
 	if (error) {
